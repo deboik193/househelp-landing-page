536ed3785a418f87d71febc62d37e68c85cec4c6
deboik193 <deboik193@gmail.com>
2022-10-12 10:46:27 +0100

    vue.config.js updated

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/househelp-landing-page/'
+    ? '/simple-vue/'
     : '/',
   transpileDependencies: true,
 });
