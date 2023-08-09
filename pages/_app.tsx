9a56a7b327cf4d26d4aa80aa4270b8ea7472e1d0
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-09 16:39:24 +0100

    house help landing page rebranded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import HeaderComponent from "../components/header";
+import Categories from "../components/categories";
 import FooterComponent from "../components/footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <HeaderComponent />
+      <hr className="hidden sm:block"/>
+      <Categories />
       <Component {...pageProps} />
       <FooterComponent />
     </>
