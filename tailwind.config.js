1d67dbd2f4273c785141f10ac6d67e40e2311722
Ikechukwu <bonifacedennis193@gmail.com>
2023-08-09 17:58:56 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+    }
   },
   plugins: [],
 }
